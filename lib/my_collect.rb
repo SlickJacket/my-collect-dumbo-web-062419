f50ef2059bd600e279a83d90fd2a8b6d12c08fd4
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 00:28:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array)
+  i=0 
+  newArray = [] 
+  while i < array.length 
+  newArray.push yield(array[i])
+  i += 1 
+end 
+  for element in array 
+    newArray.push yield element 
+  end 
+  
+  newArray
+end 
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name| 
+  name.split(" ").first 
+end 
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
