af774149a619d7fa132f0851f54ab69183727b91
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 00:29:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,7 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+
+
 def my_collect(array)
   i=0
   newArray = []
@@ -12,12 +16,12 @@ end
   newArray
 end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
 my_collect(array) do |name|
   name.split(" ").first
 end
 
-collection = ['ruby', 'javascript', 'python', 'objective-c']
+
 my_collect(collection) do |lang|
   lang.upcase
 end
