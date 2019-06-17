fc01adad82b00cf2d799c1329ec1324a0e38d270
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 00:30:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,13 +6,11 @@ def my_collect(array)
   i=0
   newArray = []
   while i < array.length
-  newArray.push
-  yield(array[i])
+  newArray.push yield(array[i])
   i += 1
 end
   for element in array
-    newArray.push
-    yield element
+    newArray.push yield element
   end
 
   newArray
