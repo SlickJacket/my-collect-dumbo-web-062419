aef1a2405008ab16c2c35d993e99faba52a1b36d
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 00:28:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,21 +1,21 @@
 def my_collect(array)
-  i=0 
-  newArray = [] 
-  while i < array.length 
+  i=0
+  newArray = []
+  while i < array.length
   newArray.push yield(array[i])
-  i += 1 
-end 
-  for element in array 
-    newArray.push yield element 
-  end 
-  
+  i += 1
+end
+  for element in array
+    newArray.push yield element
+  end
+
   newArray
-end 
+end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name| 
-  name.split(" ").first 
-end 
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
